cc839548ac57956d36a2426d592e04ffa3c9f8e7
8782682@qq.com <8782682@qq.com>
2020-12-19 23:12:09 +0800

    testtest

--- bilibili-kit/main.py
+++ bilibili-kit/main.py
@@ -29,6 +29,8 @@ base_headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36',
 }
 
+
+
 cur_threadId = 0 # used for a new QThread id
 
 try:
